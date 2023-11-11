2a62fcb0a3cb628beeac903aee049cbb22412ae4
Ahmed Radi <52893501+Ahmed-Radi@users.noreply.github.com>
2023-11-11 21:43:47 +0200

    Update ApiReducer.js

--- src/Redux/Reducers/ApiReducer.js
+++ src/Redux/Reducers/ApiReducer.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const api_key = "29549f6688d242e1ad3141808222307";
+const api_key = "75c7a39bc89a465cbab192915231111";
 
 export const getDataReducer = createAsyncThunk("weatherData/getData",
     async (args = "Egypt", thunkAPI) => {
